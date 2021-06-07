62f745f45283859529c5f69576b2597f40f428f0
igoraserpac <igoraserpac@gmail.com>
2021-06-07 14:23:13 -0300

    .project

--- pypro/settings.py
+++ pypro/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'pypro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-default_db_url = 'sqlite:///' + str(BASE_DIR) + 'db.sqlite3'
+default_db_url = 'sqlite:///' + str(BASE_DIR) + '\db.sqlite3'
 
 parse_database = partial(dj_database_url.parse, conn_max_age=600)
 
