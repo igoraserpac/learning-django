0b6b8fa555bcead0086739457bb35a97c89b2d6d
igoraserpac <igoraserpac@gmail.com>
2021-06-09 16:34:26 -0300

    tomando forma

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,13 +1,23 @@
+import pytest
 from django.test import Client
+from django.urls import reverse
 
 from pypro.django_assertions import assert_contains
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('base:home'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
 
 
-def test_title(client: Client):
-    resp = client.get('/')
+def test_title(resp):
     assert_contains(resp, '<title>Python Pro</title>')
+
+
+def test_home_link(resp):
+    assert_contains(resp, f'href="{reverse("base:home")}">Python Pro</a>')
