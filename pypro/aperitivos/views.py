a0357d715b4357298278b0159d629b176155119e
igoraserpac <igoraserpac@gmail.com>
2021-06-14 11:52:41 -0300

    commit

--- pypro/aperitivos/views.py
+++ pypro/aperitivos/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 
 def video(request, slug):
-    return render(request, 'aperitivos/video.html')
+    video = {'titulo': 'Video Aperitivo: Motivação', 'vimeo_id': '561937854'}
+    return render(request, 'aperitivos/video.html', context={'video': video})
