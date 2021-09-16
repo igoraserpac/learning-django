5371b0844f4f79547b361ec00a719a32af9443f5
igor <igoraserpac@gmail.com>
2021-09-16 11:26:08 -0300

    a

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from ordered_model.models import OrderedModel
 
-class Modulo(models.Model):
-    titulo=models.CharField(max_length=64)
+
+class Modulo(OrderedModel):
+    titulo = models.CharField(max_length=64)
+    publico = models.TextField(default='')
+    descricao = models.TextField(default='')
+    order = models.IntegerField(default=0)
+
+    class Meta(OrderedModel.Meta):
+        pass
+
+    def __str__(self):
+        return self.titulo
