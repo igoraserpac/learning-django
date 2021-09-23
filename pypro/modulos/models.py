a8c5b919ce19ef06fc70e81bac59f8057fe793b6
igor <igoraserpac@gmail.com>
2021-09-23 14:35:04 -0300

    erro arquivos estáticos

--- pypro/modulos/models.py
+++ pypro/modulos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from ordered_model.models import OrderedModel
 
 
@@ -13,3 +14,6 @@ class Modulo(OrderedModel):
 
     def __str__(self):
         return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('modulos:detalhe', kwargs={'slug': self.slug})
