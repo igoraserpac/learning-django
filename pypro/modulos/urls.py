364b4bace9b7b3eae95e160aad22fbd968e4935a
igor <igoraserpac@gmail.com>
2021-10-01 17:44:25 -0300

    breadcrumb

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -5,5 +5,6 @@ from pypro.modulos import views
 app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', views.detalhe, name='detalhe'),
+    path('', views.indice, name='indice'),
     path('aulas/<slug:slug>', views.aula, name='aula'),
 ]
